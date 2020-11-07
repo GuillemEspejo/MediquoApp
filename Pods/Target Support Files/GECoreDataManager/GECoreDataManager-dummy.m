93e04c41e135ba3be0ccb482fc48be603e667340
Guillem Espejo <guillem.espejo@luciddreamer.io>
2020-11-07 18:57:06 +0100

    Removed Pods project, was added by error.

--- Pods/Target Support Files/GECoreDataManager/GECoreDataManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GECoreDataManager : NSObject
-@end
-@implementation PodsDummy_GECoreDataManager
-@end
--- Pods/Target Support Files/Pods-MediquoApp/Pods-MediquoApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MediquoApp : NSObject
-@end
-@implementation PodsDummy_Pods_MediquoApp
-@end
